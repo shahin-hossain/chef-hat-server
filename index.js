c56ad2ba278f484b3ed4c5ed134bcf537634b628
shahin hossain <shahinhossain3@hotmail.com>
2023-12-05 20:52:56 +0600

    chefs data send in a array

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+
+const chefs = require('./data/chefData.json')
+
 const app = express();
 const port = 5000;
-
 app.use(cors());
+
+
+//server running
 app.get('/', (req, res) => {
     res.send('chef-hut server is running')
 })
+//chef data
+app.get('/chefs', (req, res) => {
+    res.send(chefs);
+})
 
+
+
+//port setup
 app.listen(port, () => {
     console.log(`chef-hut server port is ${port}`)
 })
